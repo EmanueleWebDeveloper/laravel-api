c42dbbeff1c352a60369afe3ca5e269aab194a57
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-04-08 17:11:46 +0200

    api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,15 +10,38 @@ class ProjectController extends Controller
 {
     public function index(){
 
-        $Projects = Project::all();
+        // $Projects = Project::all();
+
+        //edger loading
+        // $Projects = Project::with( 'content', 'types')->get();
+
+        //edger loading + pagination
+         $Projects = Project::with( 'type', 'technologies' )->paginate(4);
 
         // return response()->json([
         //     'name' => 'Abigail',
         //     'state' => 'California'
         //     ]);
+
         return response()->json([
             'success' => true,
             'projects' => $Projects
         ]);
+    }
+
+    public function show( $slug ){
+        $project = Project::with('type', 'technologies')->where( 'slug', $slug )->first();
+
+        if( $project ){
+            return response()->json([
+                'success' => true,
+                'projects' => $project
+            ]);
+        } else{
+            return response()->json([
+                'success' => false,
+                'error' => "il progetto non esiste"
+            ]);
+        }
     }
 }
